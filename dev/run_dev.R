7d50e96f1b69b99a071c4b6afde4df1118973e6a
masoud-faridi <masoudfaridi@modares.ac.ir>
2025-10-03 14:44:30 +0330

    initial commit 03_... completed

--- dev/run_dev.R
+++ dev/run_dev.R
@@ -1,3 +1,12 @@
+###  if you want to check devtools::check()
+
+rm(list = ls(all=TRUE))
+rm(list=ls(all.names = TRUE))
+
+
+# Sass code compilation
+sass::sass(input = sass::sass_file("inst/app/www/custom.sass"), output = "inst/app/www/custom.css", cache = NULL)
+
 # Set options here
 options(golem.app.prod = FALSE) # TRUE = production mode, FALSE = development mode
 
